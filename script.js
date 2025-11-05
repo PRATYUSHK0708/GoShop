426f9984428e26be5c6dd16188b918991d18ac38
Pratyush kumar <164166026+PRATYUSHK0708@users.noreply.github.com>
2025-11-05 23:56:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const list=document.querySelector(".navlist");
 const hamburger=document.querySelector(".fa-bars")
 
-hamburger.addEventListener("click",()=>{
-
+hamburger.addEventListener("click",(e)=>{
+    e.preventDefault();
     hamburger.classList.toggle("fa-x")
     list.classList.toggle("navlist-active")
+
 
 })
